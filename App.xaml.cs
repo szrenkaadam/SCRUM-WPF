2cc37a6e1860f081cac6b9f81a728b9fb0e15922
Ádám Szrenka <szrenka.adam@gmail.com>
2024-03-07 12:16:46 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace I_NO_NO_WANNA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
